8269c11c2ceec8c1f7f0bfaa0a93c1f5b685aaed
alfaruqii <bumbblechover@gmail.com>
2023-09-16 20:52:37 +0700

    add:  7kyu filter list

---
+++ java/7kyu/ListFiltering.java
@@ -0,0 +1,25 @@
+//Description:
+
+//In this kata you will create a function that takes a list of non-negative integers and strings and returns a new list with the strings filtered out.
+//Example
+
+//Kata.filterList(List.of(1, 2, "a", "b")) => List.of(1,2)
+//Kata.filterList(List.of(1, 2, "a", "b", 0, 15)) => List.of(1,2,0,15)
+//Kata.filterList(List.of(1, 2, "a", "b", "aasf", "1", "123", 231)) => List.of(1, 2, 231)
+
+import java.util.*;
+
+public class Kata {
+  
+  public static List<Object> filterList(final List<Object> list) {
+    ArrayList<Object> arrRes = new ArrayList<Object>();
+    for(int i=0;i<list.size();i++){
+      if(list.get(i) instanceof Integer){
+        System.out.println("bener");
+        arrRes.add(list.get(i));
+      }
+    }
+    return arrRes;
+  }
+}
+
