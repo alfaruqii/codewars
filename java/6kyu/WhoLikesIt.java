e409ef5b941c889ffadb13edd10a1ca8d2fc2712
alfaruqii <bumbblechover@gmail.com>
2023-12-29 18:07:12 +0700

    add: solved ✅ Who Likes It (java)

---
+++ java/6kyu/WhoLikesIt.java
@@ -0,0 +1,15 @@
+class Solution {
+    public static String whoLikesIt(String... names) {
+        //Do your magic here
+      if(names.length>3){
+        return names[0]+", "+names[1]+" and "+Integer.toString(names.length-2)+" others like this";
+      }else if(names.length==3){
+        return names[0]+", "+names[1]+" and "+names[2]+" like this";
+      }else if(names.length==2){
+        return names[0]+" and "+names[1]+" like this";
+      }else if(names.length==1){
+        return names[0]+" likes this";
+      }
+      return "no one likes this";
+    }
+}
